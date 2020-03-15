037e46d9197139a20d7d5b0503289d42f8e0257e
cpach96 <cpach96@gmail.com>
2020-03-15 14:47:23 -0400

    Will let you search if game does not yet exist in database

--- lib/myCliProject/api.rb
+++ lib/myCliProject/api.rb
@@ -6,10 +6,16 @@ class MyCliProject::API
             description = response["description"]
             release_date = response["released"]
             image_url = response["background_image"]
+        if description == nil
+            puts "This title has not been entered into the database yet. Please try another search"
+            MyCliProject::Cli.new.call
+        else
             MyCliProject::Game.new(game_name,description,release_date)
+        end
+        
     end
 
-    #This method controls the info pulled from RAWG API including the games description and release date. 
+    #This method controls the info pulled from RAWG API including the games description and release date. If the game doesnt exist in the database it will start a new search
 
 
 end
